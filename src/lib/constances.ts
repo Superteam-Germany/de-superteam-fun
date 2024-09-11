bd77057ae5952a97c40e0b1b5032dd265c9041bc
Janine Wagner <janine.vvagner@gmail.com>
2024-09-11 11:11:58 +0200

    refactor links and update footer links

--- src/lib/constances.ts
+++ src/lib/constances.ts
@@ -1,6 +1,22 @@
-export const UPCOMING_HACKATHON_LINK = 'https://arena.colosseum.org/register?ref=stde';
+// event links
 export const REGISTER_BUILD_STATION_KICKOFF_LINK = 'https://lu.ma/u2egdxhv?tk=NmpFYq';
 export const REGISTER_BUILD_STATION_LINK = 'https://lu.ma/buildstation';
 export const REGISTER_BUILD_STATION_DEMO_DAY_LINK = 'https://lu.ma/berlindemoday';
 
+// Hackathon
+export const PROJECT_SUBMISSION_LINK = 'https://tally.so/r/mD5Q7X';
+export const UPCOMING_HACKATHON_LINK = 'https://arena.colosseum.org/register?ref=stde';
+
+// Locations
 export const WEB3_HUB_LOCATION_LINK = 'https://goo.gl/maps/bsnPMX1QNZ1Bxa7w8';
+
+// Superteam Resources and links
+export const SUPERTEAM_GLOBAL_LINK = 'https://superteam.fun';
+export const RESOURCES_AND_GUILDS_LINK = 'https://docs.superteamde.fun/superteam-germany-guilds';
+
+// Socials
+export const SOCIAL_X_LINK = 'https://twitter.com/SuperteamDE';
+export const SOCIAL_DISCORD_LINK = 'https://discord.gg/CVwJhHgFfF';
+export const SOCIAL_TELEGRAM_LINK = 'https://t.me/SuperteamDE';
+export const SOCIAL_YOUTUBE_LINK = 'https://www.youtube.com/@SuperteamDE';
+
