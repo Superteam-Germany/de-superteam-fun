276eda6227ff079c8a3aa1a6573c6813c4cca348
MikePawel <radlinski.mikolaj@gmail.com>
2025-03-13 23:37:16 +0100

    changed events display to Luma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,30 @@
-import Hero from '../sections/home/hero';
-import WhatIsSuperteam from '../sections/home/what-is-superteam';
-import FadeInDiv from '@/components/fade-in-div';
-import { Suspense } from 'react';
-import WhatWeDo from '../sections/home/what-we-do';
-import NewsletterSection from '../sections/home/newsletter-section';
-import { Events } from '../sections/home/events';
-import type { Metadata } from 'next';
-import TrustedBy from '../sections/home/trusted-by';
+import Hero from "../sections/home/hero";
+import WhatIsSuperteam from "../sections/home/what-is-superteam";
+import FadeInDiv from "@/components/fade-in-div";
+import { Suspense } from "react";
+import WhatWeDo from "../sections/home/what-we-do";
+import NewsletterSection from "../sections/home/newsletter-section";
+import { Events } from "../sections/home/events";
+import type { Metadata } from "next";
+import TrustedBy from "../sections/home/trusted-by";
 
 export const metadata: Metadata = {
-  title: 'Superteam Germany',
-  description:
-    'The heartbeat of Germany\'s Solana community.',
-}
+  title: "Superteam Germany",
+  description: "The heartbeat of Germany's Solana community.",
+};
 
 export default function Home() {
   return (
-    <div className='min-h-screen'>
+    <div className="min-h-screen">
       <FadeInDiv>
         <Hero />
       </FadeInDiv>
       <Suspense>
         <WhatIsSuperteam />
       </Suspense>
-      <Events />
+      {/* <Events /> */}
       <TrustedBy />
-      <WhatWeDo/>
+      <WhatWeDo />
       {/* <NewsletterSection /> */}
     </div>
   );
