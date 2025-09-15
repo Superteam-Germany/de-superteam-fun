4a386b8edce9f6e25fc659dacfc4befa86e56d0e
MikePawel <radlinski.mikolaj@gmail.com>
2025-09-15 17:47:01 +0200

    tg link updated

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,26 +1,31 @@
 // event links
-export const REGISTER_BUILD_STATION_KICKOFF_LINK = 'https://lu.ma/u2egdxhv?tk=NmpFYq';
-export const REGISTER_BUILD_STATION_LINK = 'https://lu.ma/buildstation';
-export const REGISTER_BUILD_STATION_DEMO_DAY_LINK = 'https://lu.ma/berlindemoday';
+export const REGISTER_BUILD_STATION_KICKOFF_LINK =
+  "https://lu.ma/u2egdxhv?tk=NmpFYq";
+export const REGISTER_BUILD_STATION_LINK = "https://lu.ma/buildstation";
+export const REGISTER_BUILD_STATION_DEMO_DAY_LINK =
+  "https://lu.ma/berlindemoday";
 
 // Hackathon
-export const PROJECT_SUBMISSION_LINK = 'https://tally.so/r/mD5Q7X';
-export const UPCOMING_HACKATHON_LINK = 'https://arena.colosseum.org/register?ref=stde';
+export const PROJECT_SUBMISSION_LINK = "https://tally.so/r/mD5Q7X";
+export const UPCOMING_HACKATHON_LINK =
+  "https://arena.colosseum.org/register?ref=stde";
 
 // Locations
-export const WEB3_HUB_LOCATION_LINK = 'https://goo.gl/maps/bsnPMX1QNZ1Bxa7w8';
+export const WEB3_HUB_LOCATION_LINK = "https://goo.gl/maps/bsnPMX1QNZ1Bxa7w8";
 
 // Superteam Resources and links
-export const SUPERTEAM_GLOBAL_LINK = 'https://superteam.fun';
-export const RESOURCES_AND_GUILDS_LINK = 'https://docs.superteamde.fun/superteam-germany-guilds';
-export const SUPERTEAM_EARN_LINK = 'https://earn.superteam.fun/hackathon/radar/';
+export const SUPERTEAM_GLOBAL_LINK = "https://superteam.fun";
+export const RESOURCES_AND_GUILDS_LINK =
+  "https://docs.superteamde.fun/superteam-germany-guilds";
+export const SUPERTEAM_EARN_LINK =
+  "https://earn.superteam.fun/hackathon/radar/";
 
 // Socials
-export const SOCIAL_X_LINK = 'https://twitter.com/SuperteamDE';
-export const SOCIAL_DISCORD_LINK = 'https://discord.gg/CVwJhHgFfF';
-export const SOCIAL_TELEGRAM_LINK = 'https://t.me/+3Y2DX_7Plus1NzMy';
-export const SOCIAL_YOUTUBE_LINK = 'https://www.youtube.com/@SuperteamDE';
+export const SOCIAL_X_LINK = "https://twitter.com/SuperteamDE";
+export const SOCIAL_DISCORD_LINK = "https://discord.gg/CVwJhHgFfF";
+export const SOCIAL_TELEGRAM_LINK = "https://t.me/superteamgermany";
+export const SOCIAL_YOUTUBE_LINK = "https://www.youtube.com/@SuperteamDE";
 
 // Meetup
-export const MEETUP_GROUP_LINK = 'https://www.meetup.com/superteam-germany/';
-export const LUMA_GROUP_LINK = 'https://lu.ma/user/superteamde'
+export const MEETUP_GROUP_LINK = "https://www.meetup.com/superteam-germany/";
+export const LUMA_GROUP_LINK = "https://lu.ma/user/superteamde";
